0546223c28d5c111e9c0a0cc9a04f6911bcbf43d
Anisha Rosela <65699727+Ansrosel@users.noreply.github.com>
2021-03-30 13:17:35 +0700

    Update Testhelper.php

--- minggu9/ci/application/controllers/Testhelper.php
+++ minggu9/ci/application/controllers/Testhelper.php
@@ -1,9 +1,16 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
-class Testhelper extends CI_Controller
-{
-    public function index()
-    {
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Testhelper extends CI_Controller {
+    function __construct(){
+        parent::__construct();
+        $this->load->helper('html');
+    }
+    public function index(){
+        $this->load->view('view_helper');
+
+    }
+    public function template(){
         $this->load->view('template');
     }
 }
+?>
