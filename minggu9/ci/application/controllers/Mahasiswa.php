b5958ba6c81489d167406bc9bfc0e541bd055606
Ansrosel <anisharosela@gmail.com>
2021-03-30 12:46:15 +0700

    first commit

---
+++ minggu9/ci/application/controllers/Mahasiswa.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Mahasiswa extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+    }
+    public function index()
+    {
+        $data['Mahasiswa'] = $this->Mahasiswa_model->get_data();
+        $this->load->view('view_mahasiswa', $data);
+    }
+    public function profil()
+    {
+        echo "ini adalah method profil pada controller Mahasiswa";
+    }
+    public function prodi()
+    {
+        $data['Prodi'] = $this->Mahasiswa_model->get_prodi();
+        $this->load->view('view_prodi', $data);
+    }
+    public function angkatan()
+    {
+        $data['angkatan'] = $this->Mahasiswa_model->get_angkatan();
+        $this->load->view('view_angkatan', $data);
+    }
+}
